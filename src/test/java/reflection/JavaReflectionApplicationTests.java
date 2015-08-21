6a0702824f9c09bf57c7597b1979532a9dc75613
Sejin Kim <sejila@insightinfo.kr>
2015-08-21 13:10:06 +0900

    Test'

--- src/test/java/reflection/JavaReflectionApplicationTests.java
+++ src/test/java/reflection/JavaReflectionApplicationTests.java
@@ -1,9 +1,15 @@
 package reflection;
 
+import junit.framework.Assert;
 import org.junit.Test;
 
+import javax.swing.*;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import static junit.framework.Assert.*;
 
 //@RunWith(SpringJUnit4ClassRunner.class)
 //@SpringApplicationConfiguration(classes = JavaReflectionApplication.class)
@@ -14,18 +20,18 @@ public class JavaReflectionApplicationTests {
     @Test
     public void testGetClass() throws Exception {
         Class c = "foo".getClass();
-        printClassName(c);
+        assertAndPrintClassName(c);
 
         c = E.A.getClass();
-        printClassName(c);
+        assertAndPrintClassName(c);
 
         byte[] bytes = new byte[1024];
         c = bytes.getClass();
-        printClassName(c);
+        assertAndPrintClassName(c);
 
         Set<String> s = new HashSet<String>();
         c = s.getClass();
-        printClassName(c);
+        assertAndPrintClassName(c);
     }
 
     @Test
@@ -33,38 +39,54 @@ public class JavaReflectionApplicationTests {
         boolean b;
 //        Class c = b.getClass(); //compile-time error
         Class c = boolean.class;
-        printClassName(c);
+        assertAndPrintClassName(c);
 
         c = java.io.PrintStream.class;
-        printClassName(c);
+        assertAndPrintClassName(c);
 
         c = int[][][].class;
-        printClassName(c);
+        assertAndPrintClassName(c);
     }
 
     @Test
     public void testforName() throws Exception {
         Class c = Class.forName("reflection.JavaReflectionApplication");
-        printClassName(c);
+        assertAndPrintClassName(c);
 
         Class cDoubleArray = Class.forName("[D"); // double[].class
-        printClassName(cDoubleArray);
+        assertAndPrintClassName(cDoubleArray);
 
         Class cStringArray = Class.forName("[[Ljava.lang.String;"); // String[][].class
-        printClassName(cStringArray);
+        assertAndPrintClassName(cStringArray);
     }
 
     @Test
     public void testTypeField() throws Exception {
 
         Class c = Double.TYPE;
-        printClassName(c);
+        assertAndPrintClassName(c);
 
         c = Void.TYPE;
-        printClassName(c);
+        assertAndPrintClassName(c);
     }
 
-    private void printClassName(Class c) {
+    @Test
+    public void testMethodThatReturnClasses() throws Exception {
+
+        Class superClass = javax.swing.JButton.class.getSuperclass();
+        assertAndPrintClassName(superClass);
+
+        Class<?>[] classes = Character.class.getClasses();
+        assertAndPrintClassName(classes);
+    }
+
+    private void assertAndPrintClassName(Class<?>[] classes) {
+        assertNotNull(classes);
+        Arrays.asList(classes).forEach(c -> System.out.println(c));
+    }
+
+    private void assertAndPrintClassName(Class c) {
+        assertNotNull(c);
         System.out.println(c.getName());
     }
 
